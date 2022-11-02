885d3b73bd9436a5704f9b498a23f032efb14a6b
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-11-02 11:11:01 +0100

    Initial Boot project

---
+++ src/main/java/de/sample/schulung/todossample/TodosSampleApplication.java
@@ -0,0 +1,13 @@
+package de.sample.schulung.todossample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sample/schulung/todossample/TodosSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sample.schulung.todossample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
