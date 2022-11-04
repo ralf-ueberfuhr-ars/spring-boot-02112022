37acd1bf605dee902689209a2446824edca4ff67
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-11-04 14:32:40 +0100

    Use Conditional Bean to solve conflict with TodosSink implementations

--- src/main/java/de/sample/schulung/todossample/domain/TodosSinkInMemoryImpl.java
+++ src/main/java/de/sample/schulung/todossample/domain/TodosSinkInMemoryImpl.java
@@ -1,5 +1,9 @@
 package de.sample.schulung.todossample.domain;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,45 +11,56 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
 
-//@Component
-public class TodosSinkInMemoryImpl implements TodosSink {
+/*
+ * nur verwenden, wenn keine andere TodosSink im Kontext
+ * @ConditionalOnMissingBean
+ */
+@Configuration
+public class TodosSinkInMemoryImpl {
 
-    private final Map<Long, Todo> todos = new TreeMap<>();
+    @Bean
+    @ConditionalOnMissingBean(TodosSink.class)
+    TodosSink createInMemorySink() {
+        return new TodosSink() {
+            private final Map<Long, Todo> todos = new TreeMap<>();
 
-    @Override
-    public long count() {
-        return todos.size();
+            @Override
+            public long count() {
+                return todos.size();
+            }
+
+            @Override
+            public Collection<Todo> findAll() {
+                return Collections.unmodifiableCollection(todos.values());
+            }
+
+            @Override
+            public Optional<Todo> findById(long id) {
+                return Optional.ofNullable(todos.get(id));
+            }
+
+            @Override
+            public boolean exists(long id) {
+                return todos.containsKey(id);
+            }
+
+            @Override
+            public void update(Todo todo) {
+                if (todo.getId() == null) {
+                    Long newId = todos.keySet().stream()
+                      .max(Comparator.naturalOrder())
+                      .orElse(0L)
+                      + 1L;
+                    todo.setId(newId);
+                }
+                todos.put(todo.getId(), todo);
+            }
+
+            @Override
+            public void delete(long id) {
+                todos.remove(id);
+            }
+        };
     }
 
-    @Override
-    public Collection<Todo> findAll() {
-        return Collections.unmodifiableCollection(todos.values());
-    }
-
-    @Override
-    public Optional<Todo> findById(long id) {
-        return Optional.ofNullable(todos.get(id));
-    }
-
-    @Override
-    public boolean exists(long id) {
-        return todos.containsKey(id);
-    }
-
-    @Override
-    public void update(Todo todo) {
-        if(todo.getId() == null) {
-            Long newId = todos.keySet().stream()
-              .max(Comparator.naturalOrder())
-              .orElse(0L)
-              + 1L;
-            todo.setId(newId);
-        }
-        todos.put(todo.getId(), todo);
-    }
-
-    @Override
-    public void delete(long id) {
-        todos.remove(id);
-    }
 }
